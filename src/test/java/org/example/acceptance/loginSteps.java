9e40eab6243c04ef28af99f7c18d7abf915ab3ba
sama abosair <139075718+samaabosair@users.noreply.github.com>
2023-12-26 20:14:49 +0200

    Update loginSteps.java

--- src/test/java/org/example/acceptance/loginSteps.java
+++ src/test/java/org/example/acceptance/loginSteps.java
@@ -2,74 +2,80 @@ package org.example.acceptance;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.example.Registration;
+import org.example.SignIn;
 import org.junit.Assert;
 
 
 public class loginSteps {
-    Registration ob;
+    SignIn ob1;
     public loginSteps() {
-        ob =new Registration();
+        ob1=new SignIn();
     }
 
 
     @Given(": The email is {string}")
     public void the_email_is(String email) {
-        ob.setEmail(email);
+        ob1.setEmail(email);
     }
     @Given(": The password is {string}")
     public void the_password_is(String password) {
-        ob.setPassword(password);
+        ob1.setPassword(password);
+        ob1.setPassword(password);
     }
 
     @Then("customer login")
     public void customer_login() {
-        ob.customerIslLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertTrue(ob.getCustomerLogin());
+        ob1.customerIslLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertTrue(ob1.getCustomerLogin());
     }
 
     @Then("customer can not login")
     public void customer_can_not_login() {
-        ob.customerIslLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getCustomerLogin());    }
+        ob1.notFoundEmailCustomer(ob1.getEmail(),ob1.getPassword());
+        ob1.customerIslLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getCustomerLogin());    }
     @Then("customer enter false pass then can not login")
     public void customer_enter_false_pass_then_can_not_login() {
-        ob.customerIslLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getCustomerLogin());
+        ob1.notFoundPasswordCustomer(ob1.getEmail(),ob1.getPassword());
+        ob1.customerIslLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getCustomerLogin());
     }
     @Then("Admin can login")
     public void admin_can_login() {
-        ob.AdminLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(true,ob.getAdminloged());
+        ob1.AdminLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertTrue(ob1.getAdminloged());
 
     }
     @Then("Admin cant login because email wrong")
     public void admin_cant_login_because_email_wrong() {
-        ob.AdminLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getAdminloged());
+        ob1.AdminWrongEmail(ob1.getEmail(),ob1.getPassword());
+        ob1.AdminLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getAdminloged());
     }
 
     @Then("Admin cant login because pass wrong")
     public void admin_cant_login_because_pass_wrong() {
-        ob.AdminLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getAdminloged());
+        ob1.AdminWrongPass(ob1.getEmail(),ob1.getPassword());
+        ob1.AdminLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getAdminloged());
     }
 
 
     @Then("Installer can login")
     public void installer_can_login() {
-        ob.installerIsLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(true,ob.getInstallerLogin());
+        ob1.installerIsLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertTrue(ob1.getInstallerLogin());
     }
     @Then("Installer can't login")
     public void installer_can_t_login() {
-        ob.installerIsLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getInstallerLogin());
+        ob1.installerWrongEmail(ob1.getEmail(),ob1.getPassword());
+        ob1.installerIsLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getInstallerLogin());
     }
     @Then("Installer enter false pass then can't login")
     public void installer_enter_false_pass_then_can_t_login() {
-        ob.installerIsLogin(ob.getEmail(),ob.getPassword());
-        Assert.assertEquals(false,ob.getInstallerLogin());
+        ob1.installerIsLogin(ob1.getEmail(),ob1.getPassword());
+        Assert.assertFalse(ob1.getInstallerLogin());
     }
 
 
