eb58d5d223107c97abda2622f15784624ed235ec
sama abosair <139075718+samaabosair@users.noreply.github.com>
2023-12-26 20:13:52 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,11 @@ import static org.example.Registration.logger;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+    static Registration ob=new Registration();
     public static void menu() {
-        Registration ob = new Registration();
+        SignIn ob1 = new SignIn();
         int choice;
         Scanner scanner = new Scanner(System.in);
         logger.log(Level.INFO, """
@@ -25,10 +27,11 @@ public class Main {
         logger.log(Level.INFO,"Enter your choice: "+"\u001B[0m");
         choice = scanner.nextInt();
         if (choice == 1) {
-            ob.signupMenu();
-
+            signupMenu();
+            menu();
         } else if (choice == 2) {
-            ob.loginMenu();
+            ob1.loginMenu();
+            menu();
         } else if (choice == 3) {
             System.exit(0);
         } else {
@@ -36,6 +39,41 @@ public class Main {
             menu();
         }
 
+    }
+    public static void signupMenu(){
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        logger.log(Level.INFO,"\u001B[35m"+"----- Welcome to signup Page -----");
+        logger.log(Level.INFO,"Enter your name:");
+        ob.setName(scanner.next());
+        logger.log(Level.INFO,"Enter your Email:");
+        ob.setEmail(scanner.next());
+        logger.log(Level.INFO,"Enter your ID:");
+        ob.setId(scanner.nextInt());
+        logger.log(Level.INFO,"Enter your PhoneNumber:");
+        ob.setPhone(scanner.nextInt());
+        logger.log(Level.INFO,"Enter your Address e.g(Nablus/Palestine Street/Al Quds Bakery)");
+        ob.setAddress(scanner1.nextLine());
+        logger.log(Level.INFO,"Enter your Password:");
+        ob.setPassword(scanner.next());
+        logger.log(Level.INFO,"ConFirmed your Password:");
+        ob.setComPassword(scanner.next());
+        ob.searchIfEmailIsAlreadyExist(ob.getEmail());
+        if(ob.isIfEmailFound()){
+            logger.log(Level.WARNING,"This email is already exist");
+            ob.returnEnterEmail();
+
+        }
+        else if(!ob.truepass(ob.getPassword(),ob.getComPassword())){
+           ob.returnEnterPass(ob);
+        }
+        else {
+            String dD =ob.getName()+"," +ob.getEmail() + "," + ob.getPassword() + "," + ob.getAddress() + "," + ob.getId()+","+ob.getPhone()+"\n";
+            ob.storeDataToFile(dD);
+            logger.log(Level.INFO, "The registration process was completed successfully");
+
+        }
     }
 
     public static void main(String[] args) {
